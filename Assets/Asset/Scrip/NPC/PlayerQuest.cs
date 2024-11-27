ea00a912884cc2b169399ee328be1d2e9ed09c42
thuan05052005 <momspread2005@gmail.com>
2024-11-27 13:44:19 +0700

    NPC

---
+++ Assets/Asset/Scrip/NPC/PlayerQuest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerQuest : MonoBehaviour
+{
+    public List<Questitem> questItems = new List<Questitem>();
+
+    public PlayerQuestPanel playerQuestRanel;
+
+    // Nhận nhiệm vụ
+    public void TakeQuest(Questitem questItem)
+    {
+        // Kiểm tra xem nhiệm vụ đã tồn tại trong danh sách hay chưa
+        var check = questItems.FirstOrDefault(x => x.QuestItemName == questItem.QuestItemName);
+        if (check == null) // Nếu chưa có thì thêm vào danh sách
+            questItems.Add(questItem);
+        Debug.Log("Nhận nhiệm vụ: " + questItem.QuestItemName);
+        playerQuestRanel.ShowAllQuestItems(questItems);
+    }
+}
---
+++ Assets/Asset/Scrip/NPC/Questitem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Questitem : MonoBehaviour
+{
+    public string QuestItemName;// ten cua nhiem vu
+    public int QuestTargetAmount;// so luong can tim
+    public int CurrentAmount;// so luong hien tai
+    public string TargetItemTag;
+}
