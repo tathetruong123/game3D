366cc0c8dc3c61458e5420a0bed70cb9f2c36141
thuan05052005 <momspread2005@gmail.com>
2024-12-04 00:55:59 +0700

    Item

---
+++ Assets/Asset/Scrip/NPC/TabPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabPanel : MonoBehaviour
+{
+    public GameObject playerPanel; // Panel sẽ hiển thị khi nhấn Tab
+    private bool isPanelVisible = false; // Trạng thái hiển thị panel
+
+    void Update()
+    {
+        // Kiểm tra nếu người chơi nhấn phím Tab
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            isPanelVisible = !isPanelVisible; // Đảo ngược trạng thái hiển thị
+            playerPanel.SetActive(isPanelVisible); // Bật hoặc tắt Player Panel
+        }
+    }
+}
